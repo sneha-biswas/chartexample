2cdaa9d68dee3972f0bf64084f22c56941484854
sneha.biswas <sneha.biswas@speridian.com>
2021-10-25 14:58:52 +0530

    Bar Char Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
 import { Component } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+  ctx: any;
   title = 'chartexample';
+
+  ngOnInit(): void {
+    this.getChart();
+  }
+
+
+  getChart(): void {   
+    this.ctx = document.getElementById('myChart');
+    const myChart = new Chart(this.ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange','Black'],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(255, 159, 64, 0.2)',
+            'rgba(255, 205, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(201, 203, 207, 0.2)'
+          ],
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(255, 159, 64)',
+            'rgb(255, 205, 86)',
+            'rgb(75, 192, 192)',
+            'rgb(54, 162, 235)',
+            'rgb(153, 102, 255)',
+            'rgb(201, 203, 207)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      },
+    });
+  }
+
 }
